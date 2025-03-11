a276dc2465a4e80f6c2ff53e6763bae0fc14c24c
Hussein <a.hussein@wustl.edu>
2025-03-11 07:40:28 -0500

    add gradient function for noise generation

--- main.cu
+++ main.cu
@@ -92,6 +92,29 @@ __device__ float lerp(float a, float b, float t) {
     return a + t * (b - a);
 }
 
+__device__ float grad(int hash, float x, float y, float z) {
+    switch(hash & 0xF)
+    {
+        case 0x0: return  x + y;
+        case 0x1: return -x + y;
+        case 0x2: return  x - y;
+        case 0x3: return -x - y;
+        case 0x4: return  x + z;
+        case 0x5: return -x + z;
+        case 0x6: return  x - z;
+        case 0x7: return -x - z;
+        case 0x8: return  y + z;
+        case 0x9: return -y + z;
+        case 0xA: return  y - z;
+        case 0xB: return -y - z;
+        case 0xC: return  y + x;
+        case 0xD: return -y + z;
+        case 0xE: return  y - x;
+        case 0xF: return -y - z;
+        default: return 0; // never happens
+    }
+}
+
 // ======================== Main ========================
 int main() {
     printf("Hello World from CPU!\n");
